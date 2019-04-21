38484bfedc0b234066393a85c621e126ae405e51
VHSCV <47062362+HimaR151299@users.noreply.github.com>
2019-04-21 15:32:19 +0530

    Add files via upload

---
+++ DFT1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 14:00:45 2019
+
+@author: rgukt
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+import cmath as cm
+x=[1,1,1,1]
+N=input("Enter the number of DFT points: ")
+h=cm.sqrt(-1)
+k=np.arange(0,N)
+n1=np.arange(0,N)
+
+if len(x)<N:
+    q =N-len(x)
+    x=np.append(x,np.zeros(q))
+    
+Xk=[]
+for i in k:
+    sum=0;
+    for j in n1:
+        sum+=x[j]*np.exp((-h*2*np.pi*i*j)/(N))
+    Xk=np.append(Xk,sum)
+print Xk
+print len(Xk)
+
